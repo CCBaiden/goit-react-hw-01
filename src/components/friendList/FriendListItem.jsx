19b60ec3b43d91e74c2c5f5ddbe29c5920e542e1
CCBaiden <ceren.baiden@gmail.com>
2025-02-19 22:06:26 +0300

    added friendlistitem component

---
+++ src/components/friendList/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FriendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className="friend-list-item">
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p className={isOnline ? 'online' : 'offline'}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
